cbec59686b18d75309d01194a8d28b3fcbf8eedc
Peter Ahlgren <ahlgren1234@gmail.com>
2024-03-17 19:43:42 +0100

    Updated REadme file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import { createPortal } from 'react-dom'
 import './App.css';
+import Modal from './components/Modal';
 
 function App() {
+
+  const [modalOpen, setModalOpen] = useState(false)
+  const [message, setMessage] = useState("")
+
+  const openModal = () => {
+    setModalOpen(true)
+    setMessage("")
+  }
+
+  const handleButtonClick = (value) => {
+    setModalOpen(false)
+    setMessage(value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { message }
+      <button className="btn btn-open" onClick={ () => openModal() }>Open</button>
+      {modalOpen && (
+        createPortal(<Modal onSubmit={handleButtonClick} onCancel={handleButtonClick} onClose={handleButtonClick}>
+          <h1>This is the title</h1>
+          <p>This is the modal description.</p>
+        </Modal>, document.body)
+      )}
     </div>
   );
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Modal.css'
+
+const Modal = ({ onSubmit, onCancel, onClose, children }) => {
+  return (
+    <div className='modal-container' onClick={(e) => { 
+      if (e.target.className === "modal-container") {
+        onClose("The modal was closed")
+      }
+    } }>
+      <div className='modal'>
+        <div className='modal-header'>
+          <p className='close' onClick={() => onClose("The modal was closed")}>&times;</p>
+        </div>
+        <div className='modal-content'>
+          { children }
+        </div>
+        <div className='modal-footer'>
+          <button className='btn btn-submit' onClick={() => onSubmit("The Submit button was clicked")}>
+            Submit
+          </button>
+          <button className='btn btn-cancel' onClick={() => onCancel("The Cancel button was clicked")}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
